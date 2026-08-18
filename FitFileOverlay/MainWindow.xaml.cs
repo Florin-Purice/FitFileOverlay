b4a4dd53643917bc3037ec9ed062db6f047987df
Florin <dividedbyzerobh@gmail.com>
2026-08-18 16:29:47 +0300

    Keboo template base

---
+++ FitFileOverlay/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Input;
+
+namespace FitFileOverlay;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow
+{
+    public MainWindow(MainWindowViewModel viewModel)
+    {
+        DataContext = viewModel;
+        InitializeComponent();
+        CommandBindings.Add(new CommandBinding(ApplicationCommands.Close, OnClose));
+    }
+
+    private void OnClose(object sender, ExecutedRoutedEventArgs e)
+    {
+        Close();
+    }
+}
