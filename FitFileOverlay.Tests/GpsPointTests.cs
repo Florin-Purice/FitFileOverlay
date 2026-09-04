39e94a43d87485e88fb670a2e61900da0cd09fbe
Florin <dividedbyzerobh@gmail.com>
2026-09-04 16:37:28 +0300

    Add GpsPoint tests

---
+++ FitFileOverlay.Tests/GpsPointTests.cs
@@ -0,0 +1,21 @@
+﻿using FitFileOverlay.Models;
+using FitFileOverlay.Tests.Data;
+
+namespace FitFileOverlay.Tests;
+
+public class GpsPointTests
+{
+    [Test]
+    [MethodDataSource(typeof(GpsPointTestDataSources), nameof(GpsPointTestDataSources.PointsListToUnitaryScreenSpaceTestData))]
+    public async Task PointsListToUnitaryScreenSpace_ValidInput_IsInsideExpectedBounds(GpsPointPointsListToUnitaryScreenSpaceTestData testData)
+    {
+        //Arrange
+
+        //Act
+        List<(double x, double y)?> result = GpsPoint.PointsListToUnitaryScreenSpace(testData.Points, out double resultAR);
+
+        //Assert
+        await Assert.That(result).Count().IsEqualTo(testData.Points.Count);
+        await Assert.That(result).All(v => v?.x >= 0 && v?.x <= 1 && v?.y >= 0 && v?.y <= 1);
+    }
+}
