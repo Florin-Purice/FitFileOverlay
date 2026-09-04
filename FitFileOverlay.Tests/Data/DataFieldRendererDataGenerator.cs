9a71790b1da8fad8e3df78309e81fe82de45f50f
Florin <dividedbyzerobh@gmail.com>
2026-09-04 14:10:58 +0300

    Add renderer tests

---
+++ FitFileOverlay.Tests/Data/DataFieldRendererDataGenerator.cs
@@ -0,0 +1,35 @@
+﻿using FitFileOverlay.Helpers;
+using SkiaSharp;
+
+namespace FitFileOverlay.Tests.Data;
+
+public class DataFieldRendererDataGenerator : DataSourceGeneratorAttribute<DataFieldRendererTestData>
+{
+    protected override IEnumerable<Func<DataFieldRendererTestData>> GenerateDataSources(DataGeneratorMetadata dataGeneratorMetadata)
+    {
+        DataFieldRendererOptions rendererOptions = new()
+        {
+            BitmapHeight = 400,
+            BitmapWidth = 400,
+            LabelColor = SKColors.White,
+            LabelFont = new SKFont(SKTypeface.FromFamilyName("Arial"), size: 48),
+            ValueColor = SKColors.White,
+            ValueFont = new SKFont(SKTypeface.FromFamilyName("Impact", SKFontStyleWeight.Normal, SKFontStyleWidth.Normal, SKFontStyleSlant.Italic), 96),
+            UnitColor = SKColors.Orange,
+            UnitFont = new SKFont(SKTypeface.FromFamilyName("Impact", SKFontStyleWeight.Normal, SKFontStyleWidth.Normal, SKFontStyleSlant.Italic), 48),
+            LineSpacing = 20
+        };
+        yield return () => new DataFieldRendererTestData(rendererOptions, "Pace", "4:30", "/KM", "Test1.png");
+
+        rendererOptions.ValueColor = new SKColor(249, 137, 37);
+        yield return () => new DataFieldRendererTestData(rendererOptions, "HeartRate", "187", "BPM", "Test2.png");
+
+        rendererOptions.ValueColor = SKColors.White;
+        yield return () => new DataFieldRendererTestData(rendererOptions, "Distance", "4.21", "KM", "Test3.png");
+
+        rendererOptions.ValueFont = new SKFont(SKTypeface.FromFamilyName("Impact", SKFontStyleWeight.Normal, SKFontStyleWidth.Normal, SKFontStyleSlant.Italic), 48);
+        yield return () => new DataFieldRendererTestData(rendererOptions, "Timestamp", "09-Aug-26 10:12:56", string.Empty, "Test4.png");
+    }
+}
+
+public record DataFieldRendererTestData(DataFieldRendererOptions RendererOptions, string Label, string Value, string Unit, string FileName);
---
+++ FitFileOverlay.Tests/DataFieldRendererTests.cs
@@ -0,0 +1,38 @@
+﻿using FitFileOverlay.Helpers;
+using FitFileOverlay.Tests.Data;
+using SkiaSharp;
+
+namespace FitFileOverlay.Tests;
+
+public class DataFieldRendererTests
+{
+    [Test]
+    [DataFieldRendererDataGenerator]
+    public async Task Render_ValidInput_ExpectedResult(DataFieldRendererTestData testData)
+    {
+        //Arrange
+
+        //Act
+        SKBitmap result = DataFieldRenderer.Render(testData.RendererOptions, testData.Label, testData.Value, testData.Unit);
+
+        //Assert
+        await Assert.That(result).IsNotNull();
+        //save result image to file and attach artifact
+        string fileName = Path.Combine(TestContext.ResultsDirectory, "DataFieldRenderer", (testData.FileName ?? string.Empty));
+        SaveImageToFile(result, fileName);
+        TestContext.Current!.Output.AttachArtifact(fileName);
+    }
+
+    private static void SaveImageToFile(SKBitmap bitmap, string fileName)
+    {
+        string? directoryName = Path.GetDirectoryName(fileName);
+        if (directoryName != null)
+        {
+            Directory.CreateDirectory(directoryName);
+            using SKImage image = SKImage.FromBitmap(bitmap);
+            using SKData data = image.Encode(SKEncodedImageFormat.Png, quality: 80);
+            using Stream stream = File.Open(fileName, FileMode.Create);
+            data.SaveTo(stream);
+        }
+    }
+}
