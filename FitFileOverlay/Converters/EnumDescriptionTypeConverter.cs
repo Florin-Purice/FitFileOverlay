753c325568dbfa4cd51e75fd0c923b980c753103
Florin <dividedbyzerobh@gmail.com>
2026-09-07 19:10:58 +0300

    Add more generic enum to string converter
    Removed explicit DataFieldType converter

---
+++ FitFileOverlay/Converters/EnumDescriptionTypeConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using System.Reflection;
+
+namespace FitFileOverlay.Converters;
+
+public class EnumDescriptionTypeConverter(Type type) : EnumConverter(type)
+{
+    public override object? ConvertTo(ITypeDescriptorContext? ctx, CultureInfo? culture, object? value, Type destinationType)
+    {
+        if (destinationType == typeof(string) && value is Enum e)
+        {
+            var field = e.GetType().GetField(e.ToString());
+            var desc = field?.GetCustomAttribute<DescriptionAttribute>();
+            return desc?.Description ?? e.ToString();
+        }
+        return base.ConvertTo(ctx, culture, value, destinationType);
+    }
+}
