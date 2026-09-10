d8d81de25d438daee481cd4957a455738af278ef
Florin <dividedbyzerobh@gmail.com>
2026-09-10 17:29:09 +0300

    Implement basic graph renderer
    todo: value for current location (value + unit)

--- FitFileOverlay.Tests/GraphRendererTests.cs
+++ FitFileOverlay.Tests/GraphRendererTests.cs
@@ -23,6 +23,40 @@ public class GraphRendererTests
         TestContext.Current!.Output.AttachArtifact(fileName);
     }
 
+    [Test]
+    [GraphRendererRenderTrailPartDataGenerator]
+    public async Task RenderTrailPart_ValidInput_ExpectedResult(GraphRendererRenderTrailPartTestData testData)
+    {
+        //Arrange
+
+        //Act
+        SKBitmap? pathCache = null;
+        SKBitmap result = GraphRenderer.RenderTrailPart(testData.RendererOptions, testData.Values, testData.CurrentValueIndex, ref pathCache);
+
+        //Assert
+        await Assert.That(result).IsNotNull();
+        //save result image to file and attach artifact
+        string fileName = Path.Combine(TestContext.ResultsDirectory, "TestOutput", "GraphRenderer_RenderTrailPart", (testData.FileName ?? string.Empty));
+        SaveImageToFile(result, fileName);
+        TestContext.Current!.Output.AttachArtifact(fileName);
+    }
+
+    [Test]
+    [GraphRendererRenderTrailPartDataGenerator]
+    public async Task RenderTrailPart_ValidInputWithPathCache_ExpectedResult(GraphRendererRenderTrailPartTestData testData)
+    {
+        //Arrange
+
+        //Act
+        SKBitmap? pathCache = null;
+        _ = GraphRenderer.RenderTrailPart(testData.RendererOptions, testData.Values, testData.CurrentValueIndex - 1, ref pathCache);
+        SKBitmap result = GraphRenderer.RenderTrailPart(testData.RendererOptions, testData.Values, testData.CurrentValueIndex, ref pathCache);
+
+        //Assert
+        await Assert.That(pathCache).IsNotNull();
+        await Assert.That(result).IsNotNull();
+    }
+
     private static void SaveImageToFile(SKBitmap bitmap, string fileName)
     {
         string? directoryName = Path.GetDirectoryName(fileName);
